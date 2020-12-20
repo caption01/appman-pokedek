c77891f135da0c68d96e69d029211b0626eb385c
nutchapon.h <nutchapon@enres.com>
2020-12-20 22:24:21 +0700

    [6] final style

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,18 +1,41 @@
 import React from "react";
-import { Button, Row, Col } from "antd";
+import { Row, Col } from "antd";
 import styled from "styled-components";
 
 const StyledCol = styled(Col)`
+  position: relative;
   text-align: center;
+  background-color: #ec5656;
+  height: 50px;
 `;
 
-const IconAdd = ({ onClick }) => <Button onClick={onClick}>Add</Button>;
+const StyledCircle = styled.div`
+  position: absolute;
+  top: 0%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  height: 100px;
+  width: 100px;
+  background-color: #ec5656;
+  font-size: 120px;
+  color: #ffffff;
+  z-index: 2;
+  box-shadow: 0px 0.5px #d5d6dc;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
 
 const Footer = ({ onClick }) => {
   return (
     <Row>
       <StyledCol xs={24} sm={24}>
-        <IconAdd onClick={() => onClick()} />
+        <StyledCircle onClick={onClick}>+</StyledCircle>
       </StyledCol>
     </Row>
   );
